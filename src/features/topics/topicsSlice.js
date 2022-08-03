dd6487766311a99805ac9ecdb3893e58beae1328
Muifrend <andrzejzadaniadomowe1@gmail.com>
2022-08-03 13:34:43 +0200

    Topics implemented

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,21 @@
+// the topics slice
+import { createSlice } from '@reduxjs/toolkit'
+
+export const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {
+        topics: {
+            
+        }
+    },
+    reducers: {
+        addTopic(state, action) {
+            state.topics[action.payload.id] = action.payload;
+            state.topics[action.payload.id].quizIds = [];
+        }
+    }
+})
+
+export const selectTopics = (state) => state.topics.topics;
+export const { addTopic } = topicsSlice.actions
+export default topicsSlice.reducer;
